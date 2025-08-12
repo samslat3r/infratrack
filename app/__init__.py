26015dccad88d81aab791afb63809ee7ce6be6a3
Sam Slater <sslater@posteo.net>
2025-08-11 20:13:24 -0700

    Add csrf injection to be called in templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,37 @@
+# infratrack/app/__init__.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
-
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'create-an-environment-variable-for-this'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'create-an-environment-variable-for-this'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
+    # Secret + DB URL
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "replace-with-secure-key")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite+pysqlite:///infratrack.db"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Optional: quick visibility of what URL SQLAlchemy sees
+    # print("DB URL =", app.config["SQLALCHEMY_DATABASE_URI"])
+
     db.init_app(app)
     csrf.init_app(app)
-    
+
+    # Register blueprint
+    from .routes import main as main_bp
+    app.register_blueprint(main_bp)
+
     with app.app_context():
-        from . import routes, models
         db.create_all()
         
-    return app 
+    @app.context_processor
+    def inject_csrf():
+        return dict(csrf_token=csrf.generate_csrf())
+
+    return app
