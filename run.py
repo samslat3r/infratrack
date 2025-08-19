fa7507a77135e8e1c0506214e5f3ae9acc7fc16d
Sam Slater <sslater@posteo.net>
2025-08-18 22:42:40 -0700

    Make debug mode configuration via environment variable

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Don't run with debug=True in production
+    debug_mode = os.getenv('FLASK_DEBUG', '0') == '1'
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
